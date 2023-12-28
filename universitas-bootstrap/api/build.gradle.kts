55910b5d14f463302335b5b1dd9feb65fca40937
juy4556 <wnstjrtmdgus@gmail.com>
2023-12-28 19:53:30 +0900

    [UNVTS-8] feat: 프로젝트 초기 설정

---
+++ universitas-bootstrap/api/build.gradle.kts
@@ -0,0 +1,8 @@
+
+dependencies {
+    implementation(project(":universitas-common"))
+    implementation(project(":universitas-domain"))
+    implementation(project(":universitas-usecase"))
+
+    implementation(project(":universitas-infrastructure-persistence"))
+}
